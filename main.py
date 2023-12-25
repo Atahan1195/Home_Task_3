cfc5a344ab3188a7bdce35118c32761b9c572338
Atahan <atahan11951195@gmail.com>
2023-12-25 19:22:41 +0400

    Home_Task_3 - edit

--- main.py
+++ main.py
@@ -38,86 +38,42 @@
 
 
 class Discount:
-    """
-    Базовый класс для скидок
-    """
     def discount(self, total_price):
-        """
-        Метод для расчета скидки
-        :param total_price: общая стоимость заказа
-        :return: стоимость заказа со скидкой
-        """
         return total_price
 
 
 class RegularDiscount(Discount):
-    """
-    Класс для скидки по карте Regular
-    """
     def discount(self, total_price):
-        """
-        Метод для расчета скидки
-        :param total_price: общая стоимость заказа
-        :return: стоимость заказа со скидкой 5%
-        """
         return total_price * 0.95
 
 
 class SilverDiscount(Discount):
-    """
-    Класс для скидки по карте Silver
-    """
     def discount(self, total_price):
-        """
-        Метод для расчета скидки
-        :param total_price: общая стоимость заказа
-        :return: стоимость заказа со скидкой 10%
-        """
         return total_price * 0.9
 
 
 class GoldDiscount(Discount):
-    """
-    Класс для скидки по карте Gold
-    """
     def discount(self, total_price):
-        """
-        Метод для расчета скидки
-        :param total_price: общая стоимость заказа
-        :return: стоимость заказа со скидкой 15%
-        """
         return total_price * 0.85
 
 
 class Client:
-    """
-    Класс для клиента
-    """
     def __init__(self, name, discount):
-        """
-        Конструктор класса
-        :param name: имя клиента
-        :param discount: тип скидки
-        """
         self.name = name
         self.discount = discount
 
     def get_total_price(self, order):
-        """
-        Метод для расчета стоимости заказа со скидкой
-        :param order: заказ
-        :return: стоимость заказа со скидкой
-        """
         total_price = sum(order.values())
         return self.discount.discount(total_price)
 
 
-regular_discount = Client('John', RegularDiscount())
-silver_discount = Client('Bob', SilverDiscount())
-gold_discount = Client('Atahan', GoldDiscount())
+order = {'чай': 100, 'кофе': 200, 'пирожное': 300}
 
-order = {'pizza': 100, 'cola': 50, 'french fries': 70}
+client1 = Client('Вася', RegularDiscount())
+client2 = Client('Петя', SilverDiscount())
+client3 = Client('Коля', GoldDiscount())
 
-print(f'Стоимость заказа со скидкой для клиента Regular: {regular_discount.get_total_price(order)}')
-print(f'Стоимость заказа со скидкой для клиента Silver: {silver_discount.get_total_price(order)}')
-print(f'Стоимость заказа со скидкой для клиента Gold: {gold_discount.get_total_price(order)}')
+print(client1.get_total_price(order))
+print(client2.get_total_price(order))
+print(client3.get_total_price(order))
+
